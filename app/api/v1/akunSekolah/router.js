efbcb5826c2870b51a9be3cd5dc2d7d4245a3853
Sha Nia Siahaan <74463007+shaniashn@users.noreply.github.com>
2022-12-28 09:21:27 +0700

    add router.js

---
+++ app/api/v1/akunSekolah/router.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express();
+const {
+  createCMSAkun,
+  gantiStatusAkunCMS,
+  gantipasswordAkunCMS,
+} = require("./controller");
+
+const {
+  authenticateUser,
+  authorizeRoles,
+} = require("../../../middlewares/auth");
+
+router.post(
+  "/akun-sekolah",
+  authenticateUser,
+  authorizeRoles("superadmin"),
+  createCMSAkun
+);
+router.put(
+  "/akun-sekolah/ganti-status-akun/:id",
+  authenticateUser,
+  authorizeRoles("superadmin"),
+  gantiStatusAkunCMS
+);
+
+router.put(
+  "/akun-sekolah/reset-password/:id",
+  authenticateUser,
+  authorizeRoles("superadmin"),
+  gantipasswordAkunCMS
+);
+
+router.get("/akun/all-akun", getAllUser);
+
+module.exports = router;
